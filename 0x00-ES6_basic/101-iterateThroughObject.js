0e6ca5624cf2432052230efbe9f5110182371eb5
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-06-25 12:04:26 -0700

    js ES6

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,6 +1,7 @@
 export default function iterateThroughObject(reportWithIterator) {
   let employeename = '';
   let index = 1;
+
   for (let employee of reportWithIterator) {
     if (employee) {
       employeename += `${employee}`;
@@ -10,5 +11,6 @@ export default function iterateThroughObject(reportWithIterator) {
       index++;
     }
   }
+
   return employeename;
 }
