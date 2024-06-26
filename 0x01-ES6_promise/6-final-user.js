390a511a6ccacc54ce358ea757ae1e531e5157b6
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-06-25 23:25:58 -0700

    js ES6 promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,15 +3,15 @@ import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   const array = [];
-  const functionone = signUpUser(firstName, lastName);
-  const functiontwo = uploadPhoto(fileName);
+  const functionone = await signUpUser(firstName, lastName);
+  const functiontwo = await uploadPhoto(fileName);
 
   return Promise.allSettled([functionone, functiontwo]).then((res) => {
     res.forEach((r) => {
       if (r.status === 'fulfilled') {
-        array.push({ status: r.status, value: r.value });
+        array.push({ status: `${r.status}`, value: `${r.value}` });
       } else {
-        array.push({ status: r.status, value: r.reason });
+        array.push({ status: `${r.status}`, value: `${r.reason}` });
       }
     });
     return array;
