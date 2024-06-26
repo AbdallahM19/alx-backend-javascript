40a8118b9402c81c4b390d7332b31342e9082f89
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-06-25 23:18:36 -0700

    js ES6 promise

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,19 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const array = [];
+  const functionone = signUpUser(firstName, lastName);
+  const functiontwo = uploadPhoto(fileName);
+
+  return Promise.allSettled([functionone, functiontwo]).then((res) => {
+    res.forEach((r) => {
+      if (r.status === 'fulfilled') {
+        array.push({ status: r.status, value: r.value });
+      } else {
+        array.push({ status: r.status, reason: r.reason });
+      }
+    });
+    return array;
+  });
+}
