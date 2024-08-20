7746f4be3966127e6c4ad84d74e69cdc9e638824
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-08-19 20:39:23 -0700

    Executing basic

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * prints in STDOUT the string argument.
+ * @param {String} dispaly as message.
+ */
+function displayMessage(param) {
+    console.log(`${param}`)
+};
+
+module.exports = displayMessage;
