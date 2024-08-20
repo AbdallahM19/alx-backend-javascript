1721895706f08cf977b5677f34976d38c3cf5acf
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-08-20 13:00:56 -0700

    Task

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,11 +3,10 @@ const fs = require('fs');
 const countStudents = (path) => {
   try {
     const data = fs.readFileSync(path, 'utf8');
-    const lines = data.trim().split('\n');
+    const lines = data.trim().split('\n').filter((line) => line.trim() !== '');
     const header = lines[0].split(',');
     const students = lines.slice(1).map((line) => line.split(',')).filter((fields) => fields.length === header.length);
     const fieldCounts = {};
-
     students.forEach((student) => {
       const field = student[header.indexOf('field')];
       const firstname = student[header.indexOf('firstname')];
@@ -17,10 +16,11 @@ const countStudents = (path) => {
       fieldCounts[field].push(firstname);
     });
 
-    console.log(`Number of students: ${students.length}`);
+    let result = `Number of students: ${students.length}\n`;
     for (const [field, names] of Object.entries(fieldCounts)) {
-      console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+      result += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`;
     }
+    return result.trim();
   } catch (error) {
     throw new Error('Cannot load the database');
   }
