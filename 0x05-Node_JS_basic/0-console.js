080e9eb10c7798cd347bee060e125360d316c5a8
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-08-19 20:51:13 -0700

    Executing basic

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -2,9 +2,8 @@
  * prints in STDOUT the string argument.
  * @param {String} dispaly as message.
  */
-function displayMessage(param) {
+const displayMessage = (param) => {
     console.log(`${param}`);
 };
 
 module.exports = displayMessage;
-
