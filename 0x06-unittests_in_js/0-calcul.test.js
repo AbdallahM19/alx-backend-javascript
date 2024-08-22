ee391e7f90c79b4126d8b0d8704e5fbf9e463d66
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-08-22 06:09:23 +0000

    Task

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,38 +1,52 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
-const { it, describe } = require("mocha");
 
-
-describe('calculateNumber', function() {
-  it('should return 4 when input is 1 and 3', function() {
+describe('calculateNumber', () => {
+  it('should return 4 when input is 1 and 3', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
 
-  it('should return 5 when input is 1 and 3.7', function() {
+  it('should return 5 when input is 1 and 3.7', () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
 
-  it('should return 5 when input is 1.2 and 3.7', function() {
+  it('should return 5 when input is 1.2 and 3.7', () => {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
 
-  it('should return 6 when input is 1.5 and 3.7', function() {
+  it('should return 6 when input is 1.5 and 3.7', () => {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 
-  it('should return 0 when input is 0 and 0', function() {
-    assert.strictEqual(calculateNumber(0, 0), 0);
+  it('should return 3 when input is 1.0 and 2.0', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.0), 3);
   });
 
-  it('should return 1 when input is 0.4 and 0.7', function() {
-    assert.strictEqual(calculateNumber(0.4, 0.7), 1);
+  it('should return 3 when input is 1.0 and 2.4', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.4), 3);
   });
 
-  it('should return -2 when input is -1.4 and -1.4', function() {
-    assert.strictEqual(calculateNumber(-1.4, -1.4), -2);
+  it('should return 3 when input is 1.4 and 2.4', () => {
+    assert.strictEqual(calculateNumber(1.4, 2.4), 3);
   });
 
-  it('should return -2 when input is -1.5 and -1.4', function() {
-    assert.strictEqual(calculateNumber(-1.5, -1.4), -2);
+  it('should return 3 when input is 1.4 and 2.0', () => {
+    assert.strictEqual(calculateNumber(1.4, 2.0), 3);
+  });
+
+  it('should return 4 when input is 1.0 and 2.5', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.5), 4);
+  });
+
+  it('should return 6 when input is 2.6 and 2.5', () => {
+    assert.strictEqual(calculateNumber(2.6, 2.5), 6);
+  });
+
+  it('should return 5 when input is 2.6 and 2.0', () => {
+    assert.strictEqual(calculateNumber(2.6, 2.0), 5);
+  });
+
+  it('should return 5 when input is 2.499999 and 3.499999', () => {
+    assert.strictEqual(calculateNumber(2.499999, 3.499999), 5);
   });
 });
