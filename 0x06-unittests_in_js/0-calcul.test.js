c0dc6803a4e071e96866216b4a7a8d86f594ca69
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-08-22 05:29:33 +0000

    Task

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,36 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', function() {
+  it('should return 4 when input is 1 and 3', function() {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+
+  it('should return 5 when input is 1 and 3.7', function() {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+
+  it('should return 5 when input is 1.2 and 3.7', function() {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it('should return 6 when input is 1.5 and 3.7', function() {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+
+  it('should return 0 when input is 0 and 0', function() {
+    assert.strictEqual(calculateNumber(0, 0), 0);
+  });
+
+  it('should return 1 when input is 0.4 and 0.7', function() {
+    assert.strictEqual(calculateNumber(0.4, 0.7), 1);
+  });
+
+  it('should return -2 when input is -1.4 and -1.4', function() {
+    assert.strictEqual(calculateNumber(-1.4, -1.4), -2);
+  });
+
+  it('should return -2 when input is -1.5 and -1.4', function() {
+    assert.strictEqual(calculateNumber(-1.5, -1.4), -2);
+  });
+});
