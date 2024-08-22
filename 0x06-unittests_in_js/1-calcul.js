b02431e39815b65c9bd2b07a8d906aa6cfa0e55b
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-08-22 06:24:37 +0000

    Task

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,12 +1,12 @@
-function calculateNumber (type, a, b) {
+function calculateNumber(type, a, b) {
   const roundA = Math.round(a);
   const roundB = Math.round(b);
 
   if (type === 'SUM') {
     return roundA + roundB;
-  } else if (type === 'SUBTRACT') {
+  } if (type === 'SUBTRACT') {
     return roundA - roundB;
-  } else if (type === 'DIVIDE') {
+  } if (type === 'DIVIDE') {
     if (roundB === 0) {
       return 'Error';
     }
